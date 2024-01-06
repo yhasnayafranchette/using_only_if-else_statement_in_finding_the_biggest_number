e0ed84759ed7564d68e89c1ef6a585a55d7d74bc
yhasnayafranchette <yhasnayafranchettesoriano@gmail.com>
2024-01-06 21:51:26 +0800

    import tkinter

--- find_and_print_the_biggest_number.py
+++ find_and_print_the_biggest_number.py
@@ -1,4 +1,10 @@
 #Ask user to input 3 numbers. Find and print the biggest number using only if-else statement.
+import tkinter as tk
+from tkinter import messagebox
+
+root = tk.Tk()
+root.geometry("450x300")
+root.title("Number Finder")
 
 #Input three numbers from the user
 first_number = float(input("Enter the first number: "))
@@ -15,3 +21,5 @@ else:
 
 #Print the biggest number
 print("The biggest number is:", biggest_number)
+
+root.mainloop()
