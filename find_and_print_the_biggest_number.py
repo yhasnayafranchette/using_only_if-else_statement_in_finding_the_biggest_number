7ca80a4f81bac88e91ffd6e7d6a297106d24b712
yhasnayafranchette <yhasnayafranchettesoriano@gmail.com>
2024-01-06 21:05:27 +0800

    biggest number

--- find_and_print_the_biggest_number.py
+++ find_and_print_the_biggest_number.py
@@ -6,5 +6,11 @@ second_number = float(input("Enter the second number: "))
 third_number = float(input("Enter the third number: "))
 
 #Compare the numbers using if-else statement
-
+if first_number >= second_number and first_number >= third_number:
+    biggest_number = first_number
+elif second_number >= first_number and second_number >= third_number:
+    biggest_number = second_number
+else:
+    biggest_number = third_number
+     
 #Print the biggest number
