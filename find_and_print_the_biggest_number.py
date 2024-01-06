4d78150c2c17ad9ee45c1de9190af2ccda7277fc
yhasnayafranchette <yhasnayafranchettesoriano@gmail.com>
2024-01-06 21:00:18 +0800

    input numbers

--- find_and_print_the_biggest_number.py
+++ find_and_print_the_biggest_number.py
@@ -1,4 +1,10 @@
 #Ask user to input 3 numbers. Find and print the biggest number using only if-else statement.
+
 #Input three numbers from the user
+first_number = float(input("Enter the first number: "))
+second_number = float(input("Enter the second number: "))
+third_number = float(input("Enter the third number: "))
+
 #Compare the numbers using if-else statement
+
 #Print the biggest number
