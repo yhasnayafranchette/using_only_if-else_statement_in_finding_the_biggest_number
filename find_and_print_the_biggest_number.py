25e5a5a75736f665de4d62964435094aa133fe34
yhasnayafranchette <yhasnayafranchettesoriano@gmail.com>
2024-01-07 13:56:32 +0800

    window size

--- find_and_print_the_biggest_number.py
+++ find_and_print_the_biggest_number.py
@@ -23,7 +23,7 @@ def find_and_print_biggest_number():
         biggest_number_label.config(text=f"The biggest number is: {biggest_number}")
 
     except ValueError:
-        messagebox.showerror("Error", "Please enter valid numbers.")
+        messagebox.showerror("Number Finder Error", "Please enter valid numbers.")
 
 def adjust_frame_size(event):
 # Adjust frame and label sizes based on window size   
@@ -33,10 +33,10 @@ def adjust_frame_size(event):
     frame_third_number.config(padx=frame_size, pady=frame_size)
  
 root = customtkinter.CTk()
-root.geometry("680x400")
+root.geometry("985x490")
 root.title("Number Finder")
 root.config(bg="#F9F5E7")
-root.resizable(True,True)
+root.resizable(False,False)
 
 #Create frames and entry widgets for the three input numbers
 frame_first_number = customtkinter.CTkLabel(root, text=" \n \n \n Enter the first number:", font=("Times New Roman", 22),
@@ -57,16 +57,14 @@ entry_second_number.grid(row=1, column=0, padx=20, pady=20, sticky=N+S+E+W)
 frame_third_number = customtkinter.CTkLabel(root, text=" \n \n \n Enter the third number:", font=("Times New Roman", 24),
                                            text_color="black", bg_color="lightgreen", fg_color="lightgreen", width=200,
                                            height=200, compound=TOP, anchor=N)
-frame_third_number.grid(row=1, column=2, padx=20, pady=20, sticky=N+S+E+W)  # Set sticky to N+S+E+W
-
+frame_third_number.grid(row=1, column=2, padx=20, pady=20, sticky=N+S+E+W)  
 entry_third_number = tk.Entry(frame_third_number, bg="lightgreen", fg="black", bd=3, width=20, font=("Times New Roman", 18))
-entry_third_number.grid(row=1, column=0, padx=20, pady=20, sticky=N+S+E+W)  # Set sticky to N+S+E+W
-  
+entry_third_number.grid(row=1, column=0, padx=20, pady=20, sticky=N+S+E+W)  
 
 number_finder_button = tk.Button(root, text="Find Biggest Number", command=find_and_print_biggest_number, width=15, height=2, font=("Times New Roman", 16), bg="#E0AED0", fg="black")
 number_finder_button.grid(row=2, column=0, columnspan=3, pady=10)
 
-biggest_number_label = tk.Label(root, text="", font=("Times New Roman", 24), foreground="green")
+biggest_number_label = tk.Label(root, text="", font=("Times New Roman", 30), bg="#F9F5E7", foreground="red")
 biggest_number_label.grid(row=3, column=0, columnspan=3, pady=10)
 
 # Bind the resize function to the Configure event of the root window
