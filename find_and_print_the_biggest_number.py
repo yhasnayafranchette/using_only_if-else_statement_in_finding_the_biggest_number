40c9c7169b4c6992d2b76689832b6957fcecdbac
yhasnayafranchette <yhasnayafranchettesoriano@gmail.com>
2024-01-07 12:39:47 +0800

    frames and entry widgets

--- find_and_print_the_biggest_number.py
+++ find_and_print_the_biggest_number.py
@@ -38,32 +38,33 @@ root.title("Number Finder")
 root.config(bg="#F9F5E7")
 root.resizable(True,True)
 
-frame_first_number = customtkinter.CTkLabel(root, text=" \n \n \n Enter the first number:", font=("Times New Roman", 24),
-                                           text_color="black", bg_color="#F9F5E7", fg_color="lightblue", width=200,
-                                           height=200, corner_radius=20, compound=TOP, anchor=N)
+#Create frames and entry widgets for the three input numbers
+frame_first_number = customtkinter.CTkLabel(root, text=" \n \n \n Enter the first number:", font=("Times New Roman", 22),
+                                        text_color="black", bg_color="lightblue", fg_color="lightblue", width=200,
+                                        height=200, compound=TOP, anchor=N)
 frame_first_number.grid(row=1, column=0, padx=20, pady=20, sticky=W)
-entry_first_number = tk.Entry(frame_first_number, bg="lightblue", fg="blue", bd=3)
+entry_first_number = tk.Entry(frame_first_number, bg="lightblue", fg="black", bd=3, width=20, font=("Times New Roman", 18))
 entry_first_number.grid(row=1, column=0, padx=20, pady=20, sticky=W+E)
 
 frame_second_number = customtkinter.CTkLabel(root, text=" \n \n \n Enter the second number:",
-                                            font=("Times New Roman", 24), text_color="black", bg_color="#F9F5E7",
-                                            fg_color="lightpink", width=200, height=200, corner_radius=20, compound=TOP,
+                                            font=("Times New Roman", 22), text_color="black", bg_color="lightpink",
+                                            fg_color="lightpink", width=200, height=200, compound=TOP,
                                             anchor=N)
 frame_second_number.grid(row=1, column=1, padx=20, pady=20, sticky=W)
-entry_second_number = tk.Entry(frame_second_number, bg="lightpink", fg="pink", bd=3)
+entry_second_number = tk.Entry(frame_second_number, bg="lightpink", fg="black", bd=3, width=20, font=("Times New Roman", 18))
 entry_second_number.grid(row=1, column=1, padx=20, pady=20, sticky=W+E)
 
 frame_third_number = customtkinter.CTkLabel(root, text=" \n \n \n Enter the third number:", font=("Times New Roman", 24),
-                                           text_color="black", bg_color="#F9F5E7", fg_color="lightgreen", width=200,
-                                           height=200, corner_radius=20, compound=TOP, anchor=N)
+                                           text_color="black", bg_color="lightgreen", fg_color="lightgreen", width=200,
+                                           height=200, compound=TOP, anchor=N)
 frame_third_number.grid(row=1, column=2, padx=20, pady=20, sticky=W)
-entry_third_number = tk.Entry(frame_third_number, bg="lightgreen", fg="green", bd=3)
+entry_third_number = tk.Entry(frame_third_number, bg="lightgreen", fg="black", bd=3, width=20, font=("Times New Roman", 18))
 entry_third_number.grid(row=1, column=2, padx=20, pady=20, sticky=W+E)   
 
-number_finder_button = tk.Button(root, text="Find Biggest Number", command=find_and_print_biggest_number)
+number_finder_button = tk.Button(root, text="Find Biggest Number", command=find_and_print_biggest_number, width=15, height=2, font=("Times New Roman", 16), bg="#E0AED0", fg="black")
 number_finder_button.grid(row=2, column=0, columnspan=3, pady=10)
 
-biggest_number_label = tk.Label(root, text="", font=("Times New Roman", 16), foreground="green")
+biggest_number_label = tk.Label(root, text="", font=("Times New Roman", 24), foreground="green")
 biggest_number_label.grid(row=3, column=0, columnspan=3, pady=10)
 
 # Bind the resize function to the Configure event of the root window
